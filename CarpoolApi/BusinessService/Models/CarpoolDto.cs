3a7ae68fcd2deefdcd808a509d86e8ea74ef77d0
Mohr, Robin <robin.mohr@tecalliance.net>
2022-11-21 13:34:54 +0100

    Adding all necessary models, including all of their Dto´s

--- CarpoolApi/BusinessService/Models/CarpoolDto.cs
+++ CarpoolApi/BusinessService/Models/CarpoolDto.cs
@@ -4,20 +4,19 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace TecAlliance.Carpools.Data.Models
+namespace TecAlliance.Carpools.Business.Models
 {
-    public class Carpool
+    public class CarpoolDto
     {
         public int CarpoolId { get; set; }
-        public User Owner { get; set; }
+        public UserDto Owner { get; set; }
         public string StartingPoint { get; set; }
         public string EndingPoint { get; set; }
         public int FreeSpaces { get; set; }
-        public List<User> Passengers { get; set; }
+        public List<UserDto> Passengers { get; set; }
         public string Time { get; set; }
-        public bool Deleted { get; set; }
 
-        public Carpool(int carpoolId, User owner, string startingPoint, string endingPoint, int freeSpaces, List<User> passengers, string time, bool deleted)
+        public CarpoolDto(int carpoolId, UserDto owner, string startingPoint, string endingPoint, int freeSpaces, List<UserDto> passengers, string time)
         {
             CarpoolId = carpoolId;
             Owner = owner;
@@ -26,7 +25,6 @@ namespace TecAlliance.Carpools.Data.Models
             FreeSpaces = freeSpaces;
             Passengers = passengers;
             Time = time;
-            Deleted = deleted;
         }
     }
 }
--- CarpoolApi/BusinessService/Models/UserDto.cs
+++ CarpoolApi/BusinessService/Models/UserDto.cs
@@ -4,24 +4,20 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace TecAlliance.Carpools.Data.Models
+namespace TecAlliance.Carpools.Business.Models
 {
-    public class User
+    public class UserDto
     {
         public int UserID { get; set; }
-        public string Password { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public bool CanDrive { get; set; }
-        public bool Deleted { get; set; }
-        public User(int userID, string password, string firstName, string lastName, bool canDrive, bool deleted)
+        public UserDto(int userID, string password, string firstName, string lastName, bool canDrive, bool deleted)
         {
             UserID = userID;
-            Password = password;
             FirstName = firstName;
             LastName = lastName;
             CanDrive = canDrive;
-            Deleted = deleted;
         }
     }
 }
