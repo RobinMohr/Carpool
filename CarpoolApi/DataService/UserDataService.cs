559fca11a02a03da1bb584760122d79ecd783c8c
Mohr, Robin <robin.mohr@tecalliance.net>
2022-11-21 14:03:14 +0100

    Adding CRUD Methods that access the Database

--- CarpoolApi/DataService/UserDataService.cs
+++ CarpoolApi/DataService/UserDataService.cs
@@ -3,37 +3,9 @@ using TecAlliance.Carpools.Data.Models;
 
 namespace DataService
 {
-    public class UserDataService : IUserDataService
+    public class UserDataService
     {
         public string connectionString = @"Data Source=localhost;Initial Catalog=CarpoolApp;Integrated Security=True;";
-
-        public User GetUserByID(int userId)
-        {
-            using (SqlConnection connection = new SqlConnection(connectionString))
-            {
-                string queryString = $"SELECT * FROM Users WHERE UserID = {userId}";
-                SqlCommand command = new SqlCommand(queryString, connection);
-                connection.Open();
-                SqlDataReader reader = command.ExecuteReader();
-                try
-                {
-                    while (reader.Read())
-                    {
-                        return new User(Convert.ToInt32(reader["UserID"]),
-                            reader["Password"].ToString(),
-                            reader["Firstname"].ToString(),
-                            reader["Lastname"].ToString(),
-                            Convert.ToBoolean(reader["CanDrive"]),
-                            Convert.ToBoolean(reader["Deleted"]));
-                    }
-                }
-                finally
-                {
-                    reader.Close();
-                }
-                return null;
-            }
-        }
 
         public void AddUser(User user)
         {
@@ -47,7 +19,7 @@ namespace DataService
             }
         }
 
-        public List<User> ReadUserData()
+        public List<User> GetAllUser()
         {
             var users = new List<User>();
             using (SqlConnection connection = new SqlConnection(connectionString))
@@ -74,6 +46,60 @@ namespace DataService
                 }
             }
             return users;
+        }
+
+        public User GetUserByID(int userId)
+        {
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                string queryString = $"SELECT * FROM Users WHERE UserID = {userId}";
+                SqlCommand command = new SqlCommand(queryString, connection);
+                connection.Open();
+                SqlDataReader reader = command.ExecuteReader();
+                if (reader.Read())
+                {
+                    var user = new User(Convert.ToInt32(reader["UserID"]),
+                        reader["Password"].ToString(),
+                        reader["Firstname"].ToString(),
+                        reader["Lastname"].ToString(),
+                        Convert.ToBoolean(reader["CanDrive"]),
+                        Convert.ToBoolean(reader["Deleted"]));
+                    reader.Close();
+                    return user;
+                }
+                return null;
+            }
+        }
+
+        public User UpdateUser(User user)
+        {
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                if (GetUserByID(user.UserID).Password == user.Password)
+                {
+                    string queryString = $"UPDATE Users SET Firstname = {user.FirstName}, Lastname = {user.LastName}, CanDrive = {Convert.ToBoolean(user.CanDrive)} WHERE UserID = {user.UserID}";
+                    SqlCommand cmd = new SqlCommand(queryString, connection);
+                    connection.Open();
+                    cmd.BeginExecuteNonQuery();
+                }
+            }
+            return GetUserByID(user.UserID);
+        }
+
+        public User DeleteUser(int userID, string password)
+        {
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                var userToDelete = GetUserByID(userID);
+                if (userToDelete.Password == password)
+                {
+                    string queryString = $"UPDATE Users SET Deleted = {!userToDelete.Deleted} WHERE UserID = {userID}";
+                    SqlCommand cmd = new SqlCommand(queryString, connection);
+                    connection.Open();
+                    cmd.BeginExecuteNonQuery();
+                }
+            }
+            return GetUserByID(userID);
         }
     }
 }
